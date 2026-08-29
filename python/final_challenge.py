0de82a791aba613e99dd4cd8e04717ab525dfa40
srivathsanraghu <srivathsan92raghu@gmail.com>
2026-08-28 22:35:56 -0700

    Update final_challenge.py

--- python/final_challenge.py
+++ python/final_challenge.py
@@ -1,42 +1,65 @@
 import pandas as pd
 import requests
+import logging
+import json
 
-def column_rename(df):
-    df = df.rename(columns={
-    "address.city":"city",
-    'company.name':"company_name"
-    })
-    final_data = df[["id","firstName","lastName","age","email","city","company_name"]]
-    check_data(final_data)
+logging.basicConfig(
+    filename="errorlogs.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-def check_data(final_data):
-    su = final_data.isna().sum()
-    if su.sum() == 0:
-        tocsv(final_data)
-    else:
-        print("null value occured")
-        exit()
+try:
+    with open("config.json") as f:
+        config = json.load(f)
+        logging.info("URL imported")
 
-def tocsv(final_data):
-        city_count = final_data.groupby("city")["id"].count().reset_index()
-        name_sorting = final_data.sort_values(by="firstName")
-        name_sorting.index = range(1, len(name_sorting) + 1)
-        name_sorting.to_csv(r"/Users/srivathsan/Documents/Python Learning/Files/final_users.csv")
-
-        with pd.ExcelWriter(r"/Users/srivathsan/Documents/Python Learning/Files/final_users.xlsx") as writer:
-            name_sorting.to_excel(writer, sheet_name="users", index=False)
-            city_count.to_excel(writer, sheet_name="city_count", index=False)
-
-def check_code(url):
-    response = requests.get(url)
+    response = requests.get(config["url"])
     if (response.status_code == 200):
-        print("success")
+
+        logging.info("URL Reached")
         data = response.json()["users"]
+
         df = pd.json_normalize(data)
-        column_rename(df)
+        logging.info("Data normalized")
+
+        df = df.rename(columns={
+            "address.city":"city",
+            'company.name':"company_name"
+            })
+        final_data = df[["id","firstName","lastName","age","email","city","company_name"]]
+        logging.info("Columns renamed")
+
+        final_data = final_data.fillna({
+                "firstName":"Not found",
+                "lastName":"Not found",
+                "age":0,
+                "email":"Not found",
+                "city":"Not found",
+                "company_name":"Not found"
+            })
+        logging.info("cleaned the null values and program started")
+
+        su = final_data.isna().sum()
+
+        if su.sum() == 0:
+            logging.info("null not exist in data frame")
+            city_count = final_data.groupby("city")["id"].count().reset_index()
+            name_sorting = final_data.sort_values(by="firstName")
+            name_sorting.index = range(1, len(name_sorting) + 1)
+            name_sorting.to_csv(r"/Users/srivathsan/Documents/Python Learning/Files/final_users.csv")
+            
+            with pd.ExcelWriter(r"/Users/srivathsan/Documents/Python Learning/Files/final_users.xlsx") as writer:
+                    name_sorting.to_excel(writer, sheet_name="users", index=False)
+                    city_count.to_excel(writer, sheet_name="city_count", index=False)
+                    logging.info("File saved.")
+                    logging.info("Program completed")
+        else:
+            logging.info("null exist in data frame")
     else:
-        print("fail")
-
-
-url="https://dummyjson.com/users"
-check_code(url)
+        logging.error("URL failed")
+except FileNotFoundError:
+    logging.error("File not found")
+    exit()
+except Exception as e:
+     logging.error("error occurred: %s", e)
